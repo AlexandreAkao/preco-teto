8763e920bd2551ae58a71799664afbc2acf9a547
Alexandre Akira <alexandreakiraakao@gmail.com>
2024-04-14 19:32:29 -0300

    feat: add adsense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Inter } from "next/font/google";
-import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +19,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Script
+        async
+        src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3715654195056518`}
+        strategy="lazyOnload"
+        crossOrigin="anonymous"
+      />
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
