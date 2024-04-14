cf1feac7a9a77d8492f07d9a1fe757eda699f227
Alexandre Akira <alexandreakiraakao@gmail.com>
2024-04-14 19:41:40 -0300

    feat: add ad

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import AdBanner from "@/components/AdBanner";
 import Bazin from "@/components/Bazin";
 import FiisCalc from "@/components/FiisCalc";
 import MagicNumber from "@/components/MagicNumber";
@@ -6,6 +7,14 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between sm:p-24 p-5">
+      {/* <AdBanner
+        class="adsbygoogle"
+        // style="display:block"
+        data-ad-client="ca-pub-3715654195056518"
+        data-ad-slot="8273385220"
+        data-ad-format="auto"
+        data-full-width-responsive="true"
+      /> */}
       <Tabs defaultValue="method-bazin">
         <TabsList className="grid w-full grid-cols-3 gap-4">
           <TabsTrigger value="method-bazin">Método Bazin</TabsTrigger>
